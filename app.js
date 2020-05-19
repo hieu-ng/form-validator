159b1939e7267e17ec2b955114e4ba43b0794476
hieu-ng <hieutnguyen06@gmail.com>
2020-05-19 23:14:24 +1000

    Show error message

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const email = document.querySelector('email');
 const password = document.querySelector('password');
 const password2 = document.querySelector('password2');
 
+function showError(input, message) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control error';
+	const small = formControl.querySelector('small');
+	small.innerText = message;
+}
+
 form.addEventListener('submit', function (e) {
 	e.preventDefault();
 
