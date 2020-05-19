6466db0eaf57e0c6d05a7ab5cfd61a73270d3086
hieu-ng <hieutnguyen06@gmail.com>
2020-05-19 23:19:40 +1000

    Check input length

--- app.js
+++ app.js
@@ -25,6 +25,22 @@ function checkEmail(input) {
 	}
 }
 
+function checkLength(input, min, max) {
+	if (input.value.length < min) {
+		showError(
+			input,
+			`${getFieldName(input)} must be at least ${min} characters`
+		);
+	} else if (input.value.length > max) {
+		showError(
+			input,
+			`${getFieldName(input)} must be less than ${max} characters`
+		);
+	} else {
+		showSuccess(input);
+	}
+}
+
 form.addEventListener('submit', function (e) {
 	e.preventDefault();
 
