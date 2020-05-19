293d74636780764ca1bffa28adeee2578d52df5c
hieu-ng <hieutnguyen06@gmail.com>
2020-05-19 23:20:03 +1000

    Check password match

--- app.js
+++ app.js
@@ -41,6 +41,12 @@ function checkLength(input, min, max) {
 	}
 }
 
+function checkPasswordsMatch(input1, input2) {
+	if (input1.value !== input2.value) {
+		showError(input2, 'Passwords do not match');
+	}
+}
+
 form.addEventListener('submit', function (e) {
 	e.preventDefault();
 
