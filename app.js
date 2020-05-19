7857be7a6c40df90fcd739f50cd3e475938ab435
hieu-ng <hieutnguyen06@gmail.com>
2020-05-19 23:18:48 +1000

    Check valid email

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ function showSuccess(input) {
 	formControl.className = 'form-control success';
 }
 
+function checkEmail(input) {
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (re.test(input.value.trim())) {
+		showSuccess(input);
+	} else {
+		showError(input, 'Email is not valid');
+	}
+}
+
 form.addEventListener('submit', function (e) {
 	e.preventDefault();
 
@@ -23,5 +32,17 @@ form.addEventListener('submit', function (e) {
 		showError(username, 'Username is required');
 	} else {
 		showSuccess(username);
+	}
+
+	if (email.value === '') {
+		showError(email, 'Username is required');
+	} else {
+		showSuccess(email);
+	}
+
+	if (password.value === '') {
+		showError(password, 'Username is required');
+	} else {
+		showSuccess(password);
 	}
 });
